2a8ebd26122dee78ad8426c834022be8d3e12e33
Joao Pedro <joaopedrombvboas@gmail.com>
2020-02-08 13:47:08 -0300

    Updates

---
+++ precache-manifest.5d901736399164180ceebe6e598c76d3.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "2dea2742b246c6b95700d692495ee2e4",
+    "url": "/myportfolio/static/media/solutis.2dea2742.jpg"
+  },
+  {
+    "revision": "bab89ba7eba2a2ce10ba",
+    "url": "/myportfolio/static/css/main.ef5db809.chunk.css"
+  },
+  {
+    "revision": "314136164c1c0c643422",
+    "url": "/myportfolio/static/js/runtime~main.31413616.js"
+  },
+  {
+    "revision": "c40a1fa4539e8f3929bff743f7eb1fe6",
+    "url": "/myportfolio/static/media/end.c40a1fa4.jfif"
+  },
+  {
+    "revision": "ae0c48bad4eff720cae0",
+    "url": "/myportfolio/static/js/2.ae0c48ba.chunk.js"
+  },
+  {
+    "revision": "ad26e1068499c34dbabc792b78689f7e",
+    "url": "/myportfolio/static/media/eu.ad26e106.jfif"
+  },
+  {
+    "revision": "e69d1f530bcc9f75a0cc2c1551b1f145",
+    "url": "/myportfolio/static/media/skills.e69d1f53.jpg"
+  },
+  {
+    "revision": "cd3a33773cf3881bf65495c11cf81fb4",
+    "url": "/myportfolio/static/media/liberty.cd3a3377.jfif"
+  },
+  {
+    "revision": "bab89ba7eba2a2ce10ba",
+    "url": "/myportfolio/static/js/main.bab89ba7.chunk.js"
+  },
+  {
+    "revision": "d514014c627654cdb8f447838e8e10cb",
+    "url": "/myportfolio/static/media/temet.d514014c.jpg"
+  },
+  {
+    "revision": "2e7ab872cd8a7ec86a46f10bf966fe5e",
+    "url": "/myportfolio/static/media/vicentfawkes.2e7ab872.PNG"
+  },
+  {
+    "revision": "a9c87de9a3799b12e4efb24078d8b4e4",
+    "url": "/myportfolio/static/media/soslixeirasecia.a9c87de9.PNG"
+  },
+  {
+    "revision": "af0d18ace152915096567ba4f1c197c4",
+    "url": "/myportfolio/static/media/data4good.af0d18ac.jpg"
+  },
+  {
+    "revision": "1f11a1c15e3a7b191ee3a2f0adadfc05",
+    "url": "/myportfolio/static/media/notfound.1f11a1c1.jfif"
+  },
+  {
+    "revision": "ae0c48bad4eff720cae0",
+    "url": "/myportfolio/static/css/2.eb7d5d45.chunk.css"
+  },
+  {
+    "revision": "977dc8de816290baa5364d5ba0219683",
+    "url": "/myportfolio/index.html"
+  }
+];
